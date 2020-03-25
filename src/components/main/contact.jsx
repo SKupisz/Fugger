29eb28fd4ee498d0f57ea5edf489f62d8f034126
SKupisz <szym-ku@wp.pl>
2020-03-25 19:07:11 +0100

    Added link to the github profile

--- src/components/main/contact.jsx
+++ src/components/main/contact.jsx
@@ -10,6 +10,7 @@ export default class Contact extends React.Component{
                 <header className="welcome-header">Contact</header>
                 <div className="contact-ways">
                     <div className="contact-item">Email: szym-ku@wp.pl</div>
+                    <div className="contact-item">Github: <a href="https://github.com/SKupisz" target="_blank" rel="noopener noreferrer">SKupisz</a></div>
                 </div>
             </section>
         );
